5f442272c4cbf9ffd18ac6abae57f8b4b58ce17e
Daiane Arruda <daianedasilvaarruda@outlook.com>
2024-07-23 15:56:31 -0300

    header concluido

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import './styles.css'
+
+const Header = () => {
+    return (
+        <header>
+            <div className="container header">
+            <img src="https://www.ze.delivery/_next/image?url=https%3A%2F%2Fcourier-images-web.imgix.net%2Fstatic%2Fimg%2Flogo-white-text.png%3Fauto%3Dcompress%2Cformat%26fit%3Dmax%26w%3D176%26h%3D56%26dpr%3D2%26fm%3Dpng&w=256&q=75" alt="Logo do ZE Delivery" />
+            <button>Entrar</button>
+            </div>
+        </header>
+    )
+}
+
+export default Header
